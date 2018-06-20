7cf88b10a37403e03d4c439522a251fbc384b47e
Ryan <avidryanb@gmail.com>
2018-06-19 23:13:12 -0400

    The initial and probably only commit for today.

---
+++ june192018/main.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', function () {
+    new Vue({
+        el: '#leaderBoard',
+        data: {
+            rank1: '#1',
+            rank2: '#2',
+            gamerTag1: 'Ryan Is A Sith',
+            gamerTag2: 'Other Guy',
+            glory1: 698,
+            glory2: 400
+        }
+    });
+})

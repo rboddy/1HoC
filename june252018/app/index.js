0ba1c324f409c52ac48065bbcbc294ca00f17e62
Ryan <avidryanb@gmail.com>
2018-06-25 20:29:07 -0400

    Learning some error handling
    I changed the example to throw an error. Next up is rendering HTML, but for now I'm going to take a break because tomorrow I'll make a full blank directory for the next example! Woohoo!

--- june252018/app/index.js
+++ june252018/app/index.js
@@ -2,20 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-    app.use((request, response, next) => {
-        console.log(request.headers);
-        next();
-    });
-
-    app.use((request, response, next) => {
-        request.chance = Math.random();
-        next();
-    });
-
     app.get('/', (request, response) => {
-        response.json({
-            chance: request.chance
-        })
+        throw new Error('oops');
+    });
+
+    app.use((err, request, response, next) => {
+        console.log(err);
+        response.status(500).send('Something broke!');
     });
 
     app.listen(port);

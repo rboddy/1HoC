ee4ef46c1139bc6ddecc8dca73c06cb73eb1ae3b
Ryan <avidryanb@gmail.com>
2018-06-25 20:06:33 -0400

    My first node.js server
    My first node.js http server. I'm about to install express and make this a little easier.

--- june252018/app/index.js
+++ june252018/app/index.js
@@ -1,5 +1,17 @@
-// index.js
-const _ = require('lodash')
+const http = require('http');
+const port = 3000;
 
-_.assign({ 'a': 1 }, { 'b': 2 }, { 'c': 3 });
-// → { 'a': 1, 'b': 2, 'c': 3 }
+const requestHandler = (request, response) => {
+    console.log(request.url);
+    response.end('Hello Node.JS server!');
+}
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+    if(err){
+        return console.log('Something bad happened!', err)
+    }
+
+    console.log(`Server is listening on ${port}`);
+})

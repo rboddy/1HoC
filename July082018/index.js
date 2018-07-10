8a9269938f0aa26cb406f5ae74a190ecdbaa9f11
Ryan <avidryanb@gmail.com>
2018-07-09 23:50:43 -0400

    Okay let's see if these changes make it launchable?

--- July082018/index.js
+++ July082018/index.js
@@ -4,9 +4,27 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const http = require('http')
 
 //creating the express app
 const app = express();
+
+const port = 3000
+
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  response.end('Hello Node.js Server!')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
 
 //creatinging the hbs engine which I think is just a function that takes in an object with the some basic settings?
 app.engine('.hbs', exphbs({
